add51e4b5beb43a0c30c3d8aa6fce36e3caef11c
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-04-21 15:53:11 +0800

    Add logger bot logic

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
-from time import time
+from time import time, sleep
+import logging
 import requests
 from dotenv import load_dotenv
 import telegram
-import logging
 
-logging.basicConfig(level=logging.INFO)
+
+class MyLogsHandler(logging.Handler):
+    def __init__(self, logging_bot_token, chat_id):
+        super().__init__()
+        self.logging_bot = telegram.Bot(token=logging_bot_token)
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.logging_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
 
 def send_bot_message(server_response, bot, chat_id):
     host = 'https://dvmn.org'
@@ -34,27 +44,50 @@ def get_server_response(devman_token, timestamp_param):
     return response
 
 
+def create_logger(token, chat_id):
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    handler = MyLogsHandler(token, chat_id)
+    logger.addHandler(handler)
+    return logger
+
+
 def main():
-    tg_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
-    tg_chat_id = os.environ['TELEGRAM_CHAT_ID']
-    devman_token = os.environ['DEVMAN_TOKEN']
-    bot = telegram.Bot(token=tg_bot_token)
-    timestamp = time()
-    logging.info('Бот запущен.')
+    logging_bot_token = os.environ['LOGGING_BOT_TOKEN']
+    logging_chat_id = os.environ['LOGGING_BOT_CHAT_ID']
+    logger = create_logger(logging_bot_token, logging_chat_id)
+    logger.info("Бот запущен")
     while True:
         try:
-            response = get_server_response(devman_token, timestamp)
-            status = response['status']
-            if status == 'timeout':
-                timestamp = response['timestamp_to_request']
-            else:
-                timestamp = response['last_attempt_timestamp']
-                send_bot_message(response, bot, tg_chat_id)
-                continue
-        except requests.exceptions.ReadTimeout:
-            continue
-        except requests.exceptions.ConnectionError:
-            continue
+            tg_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+            tg_chat_id = os.environ['TELEGRAM_BOT_CHAT_ID']
+            devman_token = os.environ['DEVMAN_TOKEN']
+            bot = telegram.Bot(token=tg_bot_token)
+            timestamp = time()
+            while True:
+                try:
+                    response = get_server_response(devman_token, timestamp)
+                    status = response['status']
+                    if status == 'timeout':
+                        timestamp = response['timestamp_to_request']
+                    else:
+                        timestamp = response['last_attempt_timestamp']
+                        send_bot_message(response, bot, tg_chat_id)
+                        logger.info("Прислано уведомление о проверке работы.")
+                        continue
+                except requests.exceptions.ReadTimeout as readtimeout_error:
+                    logger.error("Бот упал с ошибкой:")
+                    logger.error(readtimeout_error)
+                except requests.exceptions.ConnectionError as connection_error:
+                    logger.error("Бот упал с ошибкой:")
+                    logger.error(connection_error)
+                    sleep(10)
+        except Exception as err:
+            logger.error("Бот упал с ошибкой:")
+            logger.error(err, exc_info=True)
+            sleep(20)
 
 
 if __name__ == "__main__":
